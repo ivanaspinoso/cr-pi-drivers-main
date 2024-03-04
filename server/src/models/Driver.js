c4988f715af5cb82a7abceb5f53a01b88272ee3f
Ivana <sivana361@gmail.com>
2024-03-04 14:45:29 -0300

    models changes

--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -4,9 +4,42 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Driver', {
-    name: {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    forename: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    surname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      length: 500,
+    },
+    nationality: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birthdate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    createdinDB: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    teams: {
+      type: DataTypes.STRING,
     },
   });
 };
