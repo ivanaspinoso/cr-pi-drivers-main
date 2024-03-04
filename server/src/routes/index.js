c8ab5dfa6377bf03f596404cc2babc0028ecc86f
Ivana <sivana361@gmail.com>
2024-03-04 16:19:25 -0300

    cambios routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,16 @@
 const { Router } = require("express");
+const getDrivers=require('./../controllers/getDrivers')
+const getDriverById= require('./../controllers/getDriversById')
+const getDriverByName= require('./../controllers/getDriverByName')
+const getTeams=require('./../controllers/getTeams')
+const postDriver= require('./../controllers/postDrivers')
 
 const router = Router();
+
+router.get('/drivers', getDrivers);
+router.get('/drivers/search',getDriverByName);
+router.get('/drivers/:idDriver',getDriverById);
+router.get('/teams',getTeams);
+router.post('/drivers',postDriver);
 
 module.exports = router;
